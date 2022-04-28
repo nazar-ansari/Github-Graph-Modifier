b0eeaa07e00e6fed0380ee5e1afb324a83f98922
Nazar Ansari <nazaransari06@gmail.com>
2022-04-28 22:56:09 +0530

    Loop For Previous Year Commits

--- src/RunScript.sh
+++ src/RunScript.sh
@@ -23,3 +23,37 @@ datenumber=$(date +%m)				# Results example of Apr will 04
 intdatenumber=$(expr $datenumber + 10 - 10 ) 		#To Convert Into Integer
 totaldatenumber=$(( 12 + intdatenumber ))
 commitspermonth=$(( remaining / totaldatenumber))			# To get the number of commits per month
+
+#										For Loop To execute For Previous Year
+for current_month in {1..12};do
+	if [ $current_month -eq 2 ] ;then				# If a Month Is February
+		echo -ne "\t\t\t ${months[ $(( current_month - 1 )) ]} \n" >> message.txt
+		for daysinfeb in $(seq 1 $commitspermonth);do
+			febrandom=$((RANDOM%28 + 1))
+			echo "committed on ${starting_year}-${current_month}-${febrandom}" >> message.txt ;
+			git add . &> /dev/null
+			git commit -m "created on ${starting_year}-${current_month}-${febrandom} " --date=${starting_year}-${current_month}-${febrandom} &> /dev/null
+			let remaining=remaining-1
+			echo -ne "\033[0;97m \t- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
+			echo -ne "\e[93m \t\t\t- - - - ->\tRemaining Duration is : \" $remaining s\"\t<- - - - - \n"
+			echo -ne "\033[0;97m \t- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"		
+			# sleep 0.05
+			clear
+		done			
+	else														# For Other Months
+		echo -ne "\t\t\t ${months[ $(( current_month - 1 )) ]} \n" >> message.txt
+		for daysinmonth in $(seq 1 $commitspermonth);do
+	    	othermonthrand=$((RANDOM%30 + 1))
+		    echo "committed on ${starting_year}-${current_month}-${othermonthrand}" >> message.txt ;
+			git add . &> /dev/null
+			git commit -m "created on ${starting_year}-${current_month}-${othermonthrand} " --date=${starting_year}-${current_month}-${othermonthrand} &> /dev/null
+			let remaining=remaining-1
+			echo -ne "\033[0;97m \t- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
+			echo -ne "\e[93m \t\t\t- - - - ->\tRemaining Duration is : \" $remaining s\"\t<- - - - - \n"
+			echo -ne "\033[0;97m \t- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
+			# sleep 0.05
+			clear
+		done
+	fi
+	echo -ne "\t\t\t-----------------------------------------------------\n" >> message.txt;
+done
