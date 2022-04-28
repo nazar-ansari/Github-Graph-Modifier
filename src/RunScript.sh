9dc3dc93e81d4500683668978f0553361a99708e
Nazar Ansari <nazaransari06@gmail.com>
2022-04-28 22:51:13 +0530

    Added Banner & Repo link for End-User

--- src/RunScript.sh
+++ src/RunScript.sh
@@ -3,3 +3,10 @@ year=$(date +%g)							 #22
 current_year=20$year 						#2022
 previous_year=$( expr $(date +%g) - 1) 		#21
 starting_year=20$previous_year 			#2021
+#									Banner To Get The Url Of Source
+echo -ne "\033[1;36m\t\t\t - - - - - - - - - - - - - - - - - - - - - - - - - - \n"
+echo -ne "\033[1;36m\t\t\t|    Paste The Remote U.R.L Of The Your Repository  |\n"
+echo -ne "\033[1;36m\t\t\t - - - - - - - - - - - - - - - - - - - - - - - - - - \n"
+echo ""
+read filepath			# To get The Link of Repo
+clear
